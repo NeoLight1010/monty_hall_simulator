9618ee6b8827280853585b7a7c7a6d2dac450bb5
Anthony Suárez <neolight1010@gmail.com>
2021-07-27 13:52:50 -0500

    Prettify output.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
+from rich import box
+from rich.console import Console
 from simulation import (
     Strategy,
     get_random_strategy,
-    simulate_with_random_strategy,
     Result,
     simulate_with_strategy,
 )
 from rich.live import Live
 from rich.table import Table
+from rich.align import Align, AlignMethod
 from dataclasses import dataclass
 
 
@@ -29,6 +31,9 @@ class TableData:
                 self.change_wins += 1
             elif result == Result.LOSE:
                 self.change_losses += 1
+
+    def get_total_iterations(self):
+        return self.keep_wins + self.keep_losses + self.change_wins + self.change_losses
 
     def get_keep_win_rate(self):
         try:
@@ -56,17 +61,21 @@ class TableData:
 
 
 def generate_table(data: TableData):
-    table = Table("", "Keep", "Change", title="Monty Hall Simulation")
+    table = Table(title="Monty Hall Simulation", box=box.ROUNDED)
+    
+    table.add_column("", style="bold red")
+    table.add_column("Keep", style="cyan")
+    table.add_column("Change", style="green")
 
     table.add_row(
         "Win rate",
-        str(round(data.get_keep_win_rate() * 100, 2)) + "%",
-        str(round(data.get_change_win_rate() * 100, 2)) + "%",
+        "{:.2f}%".format(data.get_keep_win_rate() * 100, 2),
+        "{:.2f}%".format(data.get_change_win_rate() * 100, 2),
     )
     table.add_row(
         "Lose rate",
-        str(round(data.get_keep_lose_rate() * 100, 2)) + "%",
-        str(round(data.get_change_lose_rate() * 100, 2)) + "%",
+        "{:.2f}%".format(data.get_keep_lose_rate() * 100, 2),
+        "{:.2f}%".format(data.get_change_lose_rate() * 100, 2),
     )
     table.add_row(
         "Iterations",
@@ -74,19 +83,23 @@ def generate_table(data: TableData):
         str(data.change_wins + data.change_losses),
     )
 
-    return table
+    return Align(table, "center")
 
 
 def main():
+    console = Console()
     data = TableData()
 
-    with Live(generate_table(data), refresh_per_second=4) as live:
-        for _ in range(1000):
+    console.clear()
+    with Live(generate_table(data), refresh_per_second=30, console=console) as live:
+        for _ in range(100000):
             strategy = get_random_strategy()
             result = simulate_with_strategy(strategy)
 
             data.update(strategy, result)
             live.update(generate_table(data))
+
+    console.print(f"[red bold]Total iterations:[/] {data.get_total_iterations()}")
 
 
 if __name__ == "__main__":
